b09b5811c88d3e83b7d14a2265271820edd7a060
Dhvani19 <dhvani.cce21@sot.pdpu.ac.in>
2024-08-22 15:06:45 +0530

    client added

---
+++ client/src/pages/Auth/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const auth = () => {
+  return (
+    <div className='h-[100vh] w-[100vw] flex items-center 
+    justify-center'>
+    <div className='h-[80vh] bg-white border-white 
+    text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl 
+    grid xl:grid-cols-2 '>
+      <div className='flex flex-col gap-10 items-center flex-col '>
+        <div className='flex flex-center justify-center'>
+          <h1 className='text-5xl font-bold md:text-6xl'>Welcome</h1>
+        </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default auth
