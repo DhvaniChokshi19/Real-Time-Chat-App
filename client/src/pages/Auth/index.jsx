110b7548d602ebf6a9988b7a1dc2c1c626c6fbf7
Dhvani19 <dhvani.cce21@sot.pdpu.ac.in>
2024-08-23 19:37:40 +0530

    auth ui added

--- client/src/pages/Auth/index.jsx
+++ client/src/pages/Auth/index.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
-
+import Background from '@/assets/login2.png';
+import victory from "@/assets/victory.svg";
+import { Input } from '@/components/ui/input';
+import { Tabs, TabsList } from '@/components/ui/tabs';
+import {TabsContent, TabsTrigger} from '@radix-ui/react-tabs';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 const auth = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const handleLogin = async ()=>{
+
+  };
+  const handleSignup =async()=>{
+
+  };
+
   return (
     <div className='h-[100vh] w-[100vw] flex items-center 
     justify-center'>
@@ -8,12 +24,65 @@ const auth = () => {
     text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl 
     grid xl:grid-cols-2 '>
       <div className='flex flex-col gap-10 items-center flex-col '>
-        <div className='flex flex-center justify-center'>
+        <div className='flex items-center justify-center'>
           <h1 className='text-5xl font-bold md:text-6xl'>Welcome</h1>
+       <img src={victory} alt="victir img" className='h-[80px]'></img>
         </div>
+        <p className='font-medium text-center'>Fill in the details to get started!</p>
+        <div className='flex items-center justify-center w-full'>
+          <Tabs className='w-3/4'>
+            <TabsList className="bg-transparent rounded-none w-full">
+              <TabsTrigger value="login"
+              className="data-[state=active]:bg-transparent text-black text-opacity-90 
+              border-b-2 rounder-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:
+              border-b-purple-500 p-3 transition-all duration-300">Login</TabsTrigger>
+              
+              <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 
+              border-b-2 rounder-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:
+              border-b-purple-500 p-3 transition-all duration-300">Signup</TabsTrigger>
+            </TabsList>
+            <TabsContent className="flex flex-col gap-5 mt-10" value="login">
+              <Input placeholder="Email" 
+              type="email"
+              className="rounded-full p-6"
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}></Input>
 
-        </div>
-      </div>
+              <Input placeholder="Password" 
+              type="password"
+              className="rounded-full p-6"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}></Input>
+              <Button className="rounded-full p-6"
+              onClick={handleLogin}>Login</Button>
+            </TabsContent>
+            <TabsContent className="flex flex-col gap-5" value="signup">
+              <Input placeholder="Email" 
+              type="email"
+              className="rounded-full p-6"
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}></Input>
+
+              <Input placeholder="Password" 
+              type="password"
+              className="rounded-full p-6"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}></Input>
+              
+              <Input placeholder="Confirm Password" 
+              type="password"
+              className="rounded-full p-6"
+              value={confirmPassword}
+              onChange={(e)=>setConfirmPassword(e.target.value)}></Input>
+            <Button className="rounded-full p-6"
+              onClick={handleSignup}>Signup</Button>
+            </TabsContent>
+          </Tabs>
+        </div>    
+      </div> 
+      <div className="hidden xl:flex justify-center items-center">
+        <img src={Background} className="h-[550px]"alt="background img"></img></div> 
+    </div>
     </div>
   )
 }
